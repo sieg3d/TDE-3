465b58eb3e10f67031dea317e97f96f302d1bd65
sieg3d <dlopes1001@gmail.com>
2023-03-08 00:03:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -58,37 +58,174 @@
 
 // }
 
-document.write(`3. Dado o valor do produto e a forma de pagamento.
-1= à vista;
-2= à prazo.
-Se o produto for pago à vista aplique um desconto de 10% antes de mostrar
-o valor final, senão informe o mesmo valor do produto.(usando switch)`)
+// document.write(`3. Dado o valor do produto e a forma de pagamento.
+// 1= à vista;
+// 2= à prazo.
+// Se o produto for pago à vista aplique um desconto de 10% antes de mostrar
+// o valor final, senão informe o mesmo valor do produto.(usando switch)`)
 
 
 
-valorProduto = +(prompt(`Qual o valor total da compra?`))
-formaPagamento = prompt(`Qual a forma de pagamento?\n1 - À vista\n2 - À prazo`)
+// valorProduto = +(prompt(`Qual o valor total da compra?`))
+// formaPagamento = prompt(`Qual a forma de pagamento?\n1 - À vista\n2 - À prazo`)
 
-switch (formaPagamento) {
-    case '1':
-        valorProduto = valorProduto * 0.90
-        document.write(`Para pagamento à vista é aplicado 10% de desconto.<br>O valor final é R$${valorProduto}`)
+// switch (formaPagamento) {
+//     case '1':
+//         valorProduto = valorProduto * 0.90
+//         document.write(`Para pagamento à vista é aplicado 10% de desconto.<br>O valor final é R$${valorProduto}`)
+//         break
+
+//     case '2':
+//         parcelas = (prompt(`Parcelamos em até 3x.\nGostaria de parcelar em quantas vezes?`))
+//         switch (parcelas) {
+//             case '2':
+//                 valorProduto = valorProduto / 2
+//                 document.write(`Sua forma de pagamento será em 2x de R$${valorProduto}.`)
+//                 break
+
+//             case '3':
+//                 valorProduto = valorProduto / 3
+//                 document.write(`Sua forma de pagamento será em 3x de R$${valorProduto}.`)
+//                 break
+//         }
+//         break
+
+//         default:
+//             (`Opção inválida.`)
+// }
+
+
+// document.write(`4. Crie um programa usando switch que leia o conceito de um aluno na
+// disciplina Desenv Web 2 e imprima seu significado, de acordo com a tabela
+// abaixo. Caso seja informado um conceito inexistente, deve ser exibida uma
+// mensagem de erro. Conceito Significado:<br>
+// A Excelente<br>
+// B Ótimo<br>
+// C Bom<br>
+// D Regular<br>
+// E Ruim<br>
+// F Nos vemos de novo ano que vem...<br>`)
+
+// conceito = prompt(`Digite o conceito do aluno:`)
+// switch (conceito) {
+
+//     case 'A':
+//         document.write(`<br>Excelente!`)
+//         break
+
+//     case 'B':
+//         document.write(`<br>Ótimo!`)
+//         break
+
+//     case 'C':
+//         document.write(`<br>Bom!`)
+//         break
+
+//     case 'D':
+//         document.write(`<br>Regular!`)
+//         break
+
+//     case 'E':
+//         document.write(`<br>Ruim!`)
+//         break
+
+//     case 'F':
+//         document.write(`<br>Nos vemos de novo ano que vem...!`)
+//         break
+
+//     default:
+//         document.write(`<br>Opção inválida.`)
+// }
+
+document.write(`5. Dada uma letra, usando switch, escreva na tela se essa letra é uma Vogal
+ou Consoante (Considerar apenas letras minúsculas).`)
+
+letra = prompt(`Digite uma letra para saber se é Vogal ou Consoante:`)
+
+switch (letra) {
+    case 'a':
+        document.write(`<br>Vogal`)
+        break
+    case 'e':
+        document.write(`<br><br>Vogal`)
+        break
+    case 'i':
+        document.write(`<br><br>Vogal`)
+        break
+    case 'o':
+        document.write(`<br><br>Vogal`)
+        break
+    case 'u':
+        document.write(`<br><br>Vogal`)
+        break
+    case 'b':
+        document.write(`Consoante`)
+        break
+    case 'c':
+        document.write(`Consoante`)
+        break
+    case 'd':
+        document.write(`Consoante`)
+        break
+    case 'e':
+        document.write(`Consoante`)
+        break
+    case 'f':
+        document.write(`Consoante`)
+        break
+    case 'g':
+        document.write(`Consoante`)
+        break
+    case 'h':
+        document.write(`Consoante`)
+        break
+    case 'j':
+        document.write(`Consoante`)
+        break
+    case 'k':
+        document.write(`Consoante`)
+        break
+    case 'l':
+        document.write(`Consoante`)
+        break
+    case 'm':
+        document.write(`Consoante`)
+        break
+    case 'n':
+        document.write(`Consoante`)
+        break
+    case 'p':
+        document.write(`Consoante`)
+        break
+    case 'q':
+        document.write(`Consoante`)
+        break
+    case 'r':
+        document.write(`Consoante`)
+        break
+    case 's':
+        document.write(`Consoante`)
+        break
+    case 't':
+        document.write(`Consoante`)
+        break
+    case 'v':
+        document.write(`Consoante`)
+        break
+    case 'w':
+        document.write(`Consoante`)
+        break
+    case 'x':
+        document.write(`Consoante`)
+        break
+    case 'y':
+        document.write(`Consoante`)
+        break
+    case 'z':
+        document.write(`Consoante`)
         break
 
-    case '2':
-        parcelas = (prompt(`Parcelamos em até 3x.\nGostaria de parcelar em quantas vezes?`))
-        switch (parcelas) {
-            case '2':
-                valorProduto = valorProduto / 2
-                document.write(`Sua forma de pagamento será em 2x de R$${valorProduto}.`)
-                break
+    default:
 
-            case '3':
-                valorProduto = valorProduto / 3
-                document.write(`Sua forma de pagamento será em 3x de R$${valorProduto}.`)
-                break
-        }
-        break
+        document.write(`Opção inválida.`)
 }
-
-
